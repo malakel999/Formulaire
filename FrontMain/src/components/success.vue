c3fa6c5451cf468d799aee5dcff06f5bc49a82e4
malakel999 <elbakalimalak312@gmail.com>
2024-10-19 22:51:20 +0100

    integration de l api

---
+++ FrontMain/src/components/success.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="x flex items-center justify-center px-4 sm:px-6 lg:px-8">
+        <div class="w-full max-w-3xl mx-auto mb-10">
+            <div class="text-center">
+                <img class="mx-auto h-10 w-auto" src="../assets/logoCode2120.png" alt="Workflow">
+            </div>
+            <div class="bg-white dark:bg-gray-800 border-l-4 border-purple-400 p-8 rounded-lg shadow-md border dark:border-gray-700 mt-10">
+                <div class="bg-white  text-gray-700 p-4 rounded-lg">
+                    <p class="text-2xl font-semibold mb-4">Félicitations !</p>
+                    <p class="text-lg ">Votre formulaire a été soumis avec succès.</p>
+                    <p class="text-lg ">Nous vous remercions pour votre participation.</p>
+                </div>
+            </div>
+            <button @click="goBack" class="mt-6 bg-purple-700 text-white px-4 py-2 justify-end rounded-lg hover:bg-purple-500">
+                Retourner à l'accueil
+            </button>
+            <!-- footer -->
+             <footer>
+                <Footer />
+             </footer>
+        </div>
+    </div>
+</template>
+<script>
+import Footer from './Footer.vue';
+export default {
+    name :'success',
+    components: {
+        Footer,
+    },
+    methods: {
+        goBack() {
+            this.$router.push('/'); // Redirige vers la page d'accueil ou une autre route de ton choix
+        }
+    }
+}
+
+</script>
--- FrontMain/src/router/index.js
+++ FrontMain/src/router/index.js
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 //import HomeView from '../views/HomeView.vue'
 import mainPage from '@/components/mainPage.vue'
 import Footer from '@/components/Footer.vue'
+import success from '@/components/success.vue'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -21,6 +23,11 @@ const router = createRouter({
           component: Footer
         }
       ]
+    },
+    {
+      path: '/success',
+      name: 'success',
+      component: success,
     },
     // {
     //   path: '/about',
